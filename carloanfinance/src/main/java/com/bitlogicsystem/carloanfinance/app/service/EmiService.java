5e8846d23e5f7c17f01f3501a6817f8b0ccb90f5
junedsiddiqui21 <juned.siddiqui21@gmail.com>
2023-07-14 14:57:39 +0530

    Emi Calculator

--- carloanfinance/src/main/java/com/bitlogicsystem/carloanfinance/app/service/EmiService.java
+++ carloanfinance/src/main/java/com/bitlogicsystem/carloanfinance/app/service/EmiService.java
@@ -1,10 +1,14 @@
 package com.bitlogicsystem.carloanfinance.app.service;
 
+import java.util.List;
+
 import com.bitlogicsystem.carloanfinance.app.model.EmiCalculator;
 
 public interface EmiService {
 
 	EmiCalculator calculatedEmi(EmiCalculator e);
+
+
 
 	
 
