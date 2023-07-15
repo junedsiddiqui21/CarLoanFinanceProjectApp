91dd04ab7aef68bc6668835f72d0c1a57e09ba06
aniket154 <daniket154@gmail.com>
2023-07-15 22:59:50 +0530

    Educationinfo class and laapclass rltn

---
+++ carloanfinance/src/main/java/com/bitlogicsystem/carloanfinance/app/model/EducationalInfo.java
@@ -0,0 +1,22 @@
+package com.bitlogicsystem.carloanfinance.app.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class EducationalInfo 
+{
+	@Id
+	@GeneratedValue(strategy =GenerationType.AUTO)
+	private int educationId;
+	private String educationType;
+}
